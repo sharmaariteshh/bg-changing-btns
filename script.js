a2103d5581c9a2f5ae806075f9167682ec20bba4
sharmaariteshh <141990332+sharmaariteshh@users.noreply.github.com>
2025-04-02 23:26:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementsByTagName("body");
+let bt = document.getElementsByTagName("button");
+
+function changeColor(colorName) {
+    document.body.style.backgroundColor = colorName;
+}
+
+
+
